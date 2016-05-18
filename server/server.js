b84871d2a7e4c44af2feaa46e75216ea79dc40ac
Mbob023 <armcgrathmedia@gmail.com>
2016-05-18 14:53:40 -0500

    updated port

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-app.set('port', (process.env.PORT || 8000));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -45,6 +43,8 @@ app.get('/*', function(req,res) {
   var file = req.params[0] || '/views/index.html';
   res.sendFile(path.join(__dirname, '/public/', file));
 });
+
+app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function() {
   console.log('Listening on port: ', app.get('port'));
